e100b318208c99d57ab3f8cbfd8a0ab5753b90f7
ZainUlBasit <zainulbasit486@gmail.com>
2024-08-10 14:53:00 +0500

    changes

--- Models/Doctor.js
+++ Models/Doctor.js
@@ -19,10 +19,11 @@ const DoctorSchema = new Schema({
     { day: String, available: Boolean, to: String, from: String },
   ],
   docUrl: String,
-  cuurent_plan: {
+  current_plan: {
     type: Number,
     enum: [1, 2, 3], // 1: Basic, 2: Premium , 3: Standard
-    required: true,
+    default: 1,
+    // required: true,
   },
   isApproved: { type: Boolean, default: false },
   Chats: [{ type: mongoose.Types.ObjectId, ref: "Chat" }],
